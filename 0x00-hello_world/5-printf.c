d99b837f4a39288a07cce1401313f4659eb48b5a
Joanfa7 <jafabre95@gmail.com>
2022-02-03 21:58:22 +0000

    add a file that prints a message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- pirnts a message
+ * Return: the message
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
