f0b86ca164be3baaa239d70684441ea54f7a25c3
Joanfa7 <jafabre95@gmail.com>
2022-02-04 18:02:41 +0000

    new file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Program that print the number fron 0-10
+ * Return: success
+ */
+
+int main(void)
+
+{
+	int Num;
+
+	for (Num = 57; Num >= 48; Num--)
+	{
+		putchar(Num);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
