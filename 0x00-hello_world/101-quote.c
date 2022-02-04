d80f063db08e7f2e5a270369829a326c8b25934b
Joanfa7 <jafabre95@gmail.com>
2022-02-04 03:16:28 +0000

    fixing Bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main- System that prints a stirng
- * Returns: 1 = off
+ * Return: 1 = off
  */
 
-int main()
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return(1);
+	write(2, "and that piece of art is useful\"", 32);
+	write(2, "- Dora Korpar, 2015-10-19\n", 26);
+	return (1);
 }
-
