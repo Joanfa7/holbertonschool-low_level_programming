ca239006865218d4419f963b0dd12d8590cd3720
Joanfa7 <jafabre95@gmail.com>
2022-02-04 16:59:45 +0000

    fix bug

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main- displays random numbers and tells if they are positive, negative or 0
+ * Return: succes
+ */
 int main(void)
 {
 	int n, LastNum;
@@ -12,11 +16,11 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("Last digit of %i and is %i greater than 5\n", n, LastNum);
+		printf("Last digit of %i is %i and is greater than 5\n", n, LastNum);
 	}
 	if (n == 0)
 	{
-		printf("Last digit of %i and is %i\n", n, LastNum);
+		printf("Last digit of %i and is %i and is 0\n", n, LastNum);
 	}
 	if (n < 6)
 	{
