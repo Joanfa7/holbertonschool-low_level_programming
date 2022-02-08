5d1f1f56281f4a6ec5be18cd025c9c6e2bf16eb1
Joanfa7 <jafabre95@gmail.com>
2022-02-08 23:16:11 +0000

    adding 4th file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha- cheks for alphabetic character
+ * @c: character
+ * Return: succes
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
