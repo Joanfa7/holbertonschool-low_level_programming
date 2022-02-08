456deaf6cd674d645e34e30356fa4cd2cae55132
Joanfa7 <jafabre95@gmail.com>
2022-02-08 23:52:46 +0000

    adding the new file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs- prints the absolute value
+ * @Num: Number
+ * Return: success
+ */
+
+int _abs(int Num)
+{
+	if (Num < 0)
+	{
+		Num = (-1) * Num;
+		return (Num);
+	}
+	else
+	{
+		return (Num);
+	}
+}
