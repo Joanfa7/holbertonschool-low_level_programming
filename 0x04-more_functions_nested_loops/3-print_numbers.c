2b18052325da684d108babbca747804581357f92
Joanfa7 <jafabre95@gmail.com>
2022-02-10 15:01:31 +0000

    adding new file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- print the numbers form 0 to 9
+ * Return: Success
+ */
+
+void print_numbers(void)
+{
+	int Num = 48;
+
+	while (Num <= 57)
+	{
+		_putchar(Num);
+		Num++;
+	}
+	_putchar('\n');
+}
