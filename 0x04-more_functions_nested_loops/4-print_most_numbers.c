641aad9f23787e2a57ed58132ffaf726b942d9c6
Joanfa7 <jafabre95@gmail.com>
2022-02-10 19:11:47 +0000

    new file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- print the numbers form 0 to 9 except 2 and 4
+ * Return: Success
+ */
+
+void print_most_numbers(void)
+{
+	int Num = 48;
+
+	while (Num <= 57)
+	{
+		if(Num != 50 && Num != 52)
+		{
+		_putchar(Num);
+		}
+		Num++;
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * 
+ *
+ */
+
+void more_numbers(void)
+{
+	int Num = 0;
+
+	while(Num < 14)
+	{
+		_putchar(Num);
+	}
+	Num++;
+	_putchar('\n');
+}
