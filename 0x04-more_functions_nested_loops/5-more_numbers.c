6dba1eb53dd5e676b787da3a8f98e98e6fa002f0
Joanfa7 <jafabre95@gmail.com>
2022-02-10 19:54:34 +0000

    adding file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 
 	for (Rep = 0; Rep < 10; Rep++)
 	{
-		for(Num = 0; Num < 15; Num++)
+		for (Num = 0; Num < 15; Num++)
 		{
-			if(Num > 9)
+			if (Num > 9)
 				_putchar((Num / 10) + '0');
 			_putchar((Num % 10) + '0');
 		}
