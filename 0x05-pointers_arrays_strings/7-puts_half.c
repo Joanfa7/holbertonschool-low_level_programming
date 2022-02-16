f9654dde00e9d33a28fca586604d91748c2a82c8
Joanfa7 <jafabre95@gmail.com>
2022-02-16 00:14:53 +0000

    fix betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ void puts_half(char *str)
 	int index = 0;
 	int length;
 
-	while (str[index] != '\0') 
+	while (str[index] != '\0')
 	{
 		index++;
 	}
 
-	if(index % 2 != 0)
+	if (index % 2 != 0)
 	{
 		length = ((index - 1) / 2);
 		for ( ; str[length] != '\0'; length++)
