0c6a0b60a08196499858b800657e32aa43a4e157
Joanfa7 <jafabre95@gmail.com>
2022-02-17 19:56:43 +0000

    add new file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string- capitalize all words
+ * @s: string
+ * Return: success
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (s[i - 1] >= 0 &&  s[i - 1] <= 47)
+			{
+				s[i] = s[i] - 32;
+			}
+			if (s[i - 1] >= 58 && s[i - 1] <= 64)
+			{
+				s[i] = s[i] - 32;
+			}
+			if (s[i - 1] >= 123 && s[i - 1] <= 126)
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s);
+}
