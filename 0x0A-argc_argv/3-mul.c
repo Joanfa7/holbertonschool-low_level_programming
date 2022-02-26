46f91c4fa4e919c5a73c8cdb3a702613a5e9333f
Joanfa7 <jafabre95@gmail.com>
2022-02-26 01:25:48 +0000

    adding new file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main- mult two numbers
+ * @argc: counter
+ * @argv: numbes
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int Num1;
+	int  Num2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+
+	Num1 = atoi(argv[1]);
+	Num2 = atoi(argv[2]);
+	result = (Num1 * Num2);
+
+	printf("%i\n", result);
+
+	return (0);
+}
