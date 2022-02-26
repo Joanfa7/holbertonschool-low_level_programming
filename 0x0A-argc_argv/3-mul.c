8c10d62f975fd30f80dba25d07519de52dbbf28e
Joanfa7 <jafabre95@gmail.com>
2022-02-26 02:01:08 +0000

    adding new file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int Num1;
-	int  Num2;
+	int Num2;
 	int result;
 
 	if (argc != 3)
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main- add numbes
+ * @argc: counter
+ * @argv: elements
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int NumChecker;
+	int Sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0 \n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (NumChecker = 0; argv[i][NumChecker] != '\0'; NumChecker++)
+		{
+			if (argv[i][NumChecker] < '0' || argv[i][NumChecker] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		Sum += atoi(argv[i]);
+	}
+	printf("%i\n", Sum);
+
+	return (0);
+}
