26958359233235885e1e3fbb0800868242a9a234
Joanfa7 <jafabre95@gmail.com>
2022-03-08 00:47:23 +0000

    update

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if(d)
+	{
+		
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef _DOG_
 #define _DOG_
 
+#include <stdio.h>
 /**
  * struct dog- dog's data
  * @name: dog's name
@@ -15,6 +16,11 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
