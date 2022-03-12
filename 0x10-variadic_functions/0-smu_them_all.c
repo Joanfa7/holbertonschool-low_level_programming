e1c8c11991023fc0b845014a2bb31b7d21b22f0a
Joanfa7 <jafabre95@gmail.com>
2022-03-12 00:17:46 +0000

    add file

---
+++ 0x10-variadic_functions/0-smu_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all- returns the sum of all its parameters
+ * @n: diferent numbers
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int a;
+	int sum = 0;
+	va_list argmuents;
+
+	va_start(argument, n);
+	for (a = 0; a < n; a++)
+	{
+		sum += va_arg(arguments, int);
+	}
+	va_end(arguments);
+	return (sum);
+}
