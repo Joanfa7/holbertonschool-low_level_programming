184b760164782c853e3afb35eeffa26c100be05f
Joanfa7 <jafabre95@gmail.com>
2022-03-18 17:26:22 +0000

    adding new file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,44 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * add_node_end- adds a node at the fist position
+ * @head:Fist node
+ * @str: name
+ * Return: success
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list newNode;
+	list_t *newNode;
+	list_t *lastNode;
 
-	NewNode = malloc(sizeof(list_t
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+	*head = newNode;
+	return (*head);
+	}
+
+	else
+	{
+		lastNode = *head;
+
+		while (lastNode->next != NULL)
+		{
+			lastNode = lastNode->next;
+		}
+		lastNode->next = newNode;
+		return (lastNode);
+	}
+	return (NULL);
 }
