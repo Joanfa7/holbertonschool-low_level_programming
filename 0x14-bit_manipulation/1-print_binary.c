35f28925f6433396342efd85e010b2d4ddef5a78
Joanfa7 <jafabre95@gmail.com>
2022-03-24 22:19:19 +0000

    adding new file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_binary - function prints out binary number
+ * @n: integer
+ */
+
+void print_binary(unsigned long int n)
+{
+	for (; n > 1;)
+	{
+		n = n >> 1;
+		_putchar((n & 1) + 48);
+	}
+
+	if (n & 1)
+		_putchar('1');
+
+	else
+		_putchar('0');
+}
+
+
