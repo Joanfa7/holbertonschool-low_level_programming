f113ffcaed0b2047b0400b2ccc595a61429ae266
Joanfa7 <jafabre95@gmail.com>
2022-03-24 22:29:54 +0000

    fix bug

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,11 @@
 
 void print_binary(unsigned long int n)
 {
-	for (; n > 1;)
-	{
-		n = n >> 1;
-		_putchar((n & 1) + 48);
-	}
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n & 1)
-	{
-		_putchar('1');
-	}
-
-	else
-	{
-		_putchar('0');
-	}
+	n = n & 1;
+	_putchar(n + '0');
 }
 
 
