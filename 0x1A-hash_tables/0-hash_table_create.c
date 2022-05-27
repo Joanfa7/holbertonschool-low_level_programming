e338468f43bbda3e108cab54ce2a5d2f845c30e1
Joanfa7 <jafabre95@gmail.com>
2022-05-27 22:02:40 +0000

    quick fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,16 +15,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t * )  * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
-	return(table);
+	return (table);
 }
 
